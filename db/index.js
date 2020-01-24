e00edac4dcdd48687918502295809246de15e52f
naszzz <naszagorov@gmail.com>
2020-01-23 16:00:58 -0800

    db schema

---
+++ db/index.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/product');
+
+let productSchema = mongoose.Schema({
+  id: Number,
+  sellerName: String,
+  itemDescription: String,
+  itemPrice: Number,
+  itemSpecs: String,
+  shippingTime: String,
+  shippingLoc: String
+
+})
+
+let Product = mongoose.model('Product', productSchema);
+
+module.exports.Product = Product;
+
