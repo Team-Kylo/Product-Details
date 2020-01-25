96e16069a08353affcf28599f4940382574f632e
naszzz <naszagorov@gmail.com>
2020-01-24 16:50:18 -0800

    styled components

--- client/components/AddToCart.jsx
+++ client/components/AddToCart.jsx
@@ -1,18 +1,35 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Button = styled.button`
+  width: 50%;
+  background: black;
+  color: white;
+`;
+
+const DropDown = styled.select`
+  width: 50%;
+  option {
+    text-align: center;
+  }
+`;
 
 
 var AddToCart = () => {
   return(
     <div>
       <div>Quantity</div>
-      <select>
+      <DropDown>
+
         <option>1</option>
         <option>2</option>
         <option>3</option>
         <option>4</option>
-      </select>
+
+      </DropDown>
       <div>
-      <button>Add to cart</button>
+      {/* <button>Add to cart</button> */}
+      <Button>Add to cart</Button>
       </div>
       <div>Don't miss out. There's only 4 available and 4 other people have this in their cart right now.</div>
     </div>
--- client/components/Item.jsx
+++ client/components/Item.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import AddToCart from './AddToCart.jsx';
+import styled from 'styled-components';
+
+
+const StyledText = styled.div`
+  color: palevioletred;
+`;
+
+
 
 var Item = ({sellerName, itemDesc, itemPrice}) => {
 
   return(
     <div className="product">
-      <div className="seller">{sellerName}</div>
+      {/* <div className="seller">{sellerName}</div> */}
+      <StyledText>{sellerName}</StyledText>
       <div className="desc">{itemDesc}</div>
       <div className="price">{`$${itemPrice}.00`}</div>
       <div>
