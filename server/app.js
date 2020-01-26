876a01f2c216c0423efc687d101fccc183abd6e7
naszzz <naszagorov@gmail.com>
2020-01-26 09:32:34 -0800

    refactor server and app

---
+++ server/app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+let app = express();
+let db = require('../db');
+
+
+app.use(express.json());
+app.use(express.static('./public'));
+
+
+app.get('/:id', (req, res) => {
+
+  db.fetchDocs((err, data) => {
+    if (err) {
+      res.status(400).json(err);
+      return;
+    }
+    var targetId = req.params.id;
+    var targetData;
+
+    for (var el of data) {
+      if (parseInt(el.id) === parseInt(targetId)) {
+        console.log('trrruueee');
+        targetData = el;
+        break;
+      }
+    }
+    res.status(200).json(targetData);
+  })
+
+
+});
+
+
+module.exports = app;
--- server/server.js
+++ server/server.js
@@ -1,36 +1,8 @@
-const express = require('express');
-let app = express();
+
+let app = require('./app.js');
 let port = 3000;
-let db = require('../db');
-
-app.use(express.json());
-
-app.use(express.static('./public'));
-
-app.get('/:id', (req, res) => {
-
-  db.fetchDocs((err, data) => {
-    if (err) {
-      res.status(400).json(err);
-      return;
-    }
-    var targetId = req.params.id;
-    var targetData;
-
-    for (var el of data) {
-      if (parseInt(el.id) === parseInt(targetId)) {
-        console.log('trrruueee');
-        targetData = el;
-        break;
-      }
-    }
-    res.status(200).json(targetData);
-  })
-
-
-});
 
 
 app.listen(port, () => {
   console.log('app listening on port 3000');
-})
+});
