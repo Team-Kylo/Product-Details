f80c683b09d377463cb1159dd477de4e7c043017
naszzz <naszagorov@gmail.com>
2020-01-30 16:09:33 -0800

    show more details button

--- client/components/Details.jsx
+++ client/components/Details.jsx
@@ -1,24 +1,64 @@
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable class-methods-use-this */
 /* eslint-disable react/prop-types */
 /* eslint-disable arrow-body-style */
 import React from 'react';
 
-const Details = ({ itemSpecs }) => {
-  console.log(itemSpecs.cm);
+class Details extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      clicked: false,
+      buttonText: false,
+    };
+  }
 
-  return (
-    <div>
-      <h4>Item Details</h4>
-      <p>Handmade</p>
-      <p>
-        {itemSpecs.cm}
-        cm
-      </p>
-      <p>{itemSpecs.materialA}</p>
-      <p>{itemSpecs.materialB}</p>
-      <p>{itemSpecs.materialC}</p>
-      <p>{itemSpecs.materialD}</p>
-    </div>
-  );
-};
+  clickLearnMore() {
+    const toggled = !this.state.buttonText;
+    this.setState({
+      clicked: true,
+      buttonText: toggled,
+    });
+  }
+
+  render() {
+    const { itemSpecs } = this.props;
+    const { clicked, buttonText } = this.state;
+    let item = null;
+    if (clicked) {
+      item = (
+        <form>
+          <p>{itemSpecs.materialC}</p>
+          <p>{itemSpecs.materialD}</p>
+        </form>
+      );
+    } else {
+      item = null;
+    }
+    let button = '';
+    if (buttonText) {
+      button = 'Less';
+    } else {
+      button = 'Learn More About This Item';
+    }
+
+    return (
+      <div>
+        <h4>Item Details</h4>
+        <p>Handmade</p>
+        <p>
+          {itemSpecs.cm}
+          cm
+        </p>
+        <p>{itemSpecs.materialA}</p>
+        <p>{itemSpecs.materialB}</p>
+        <div>
+          {item}
+        </div>
+        <button type="submit" onClick={() => this.clickLearnMore()}>{button}</button>
+      </div>
+    );
+  }
+}
 
 export default Details;
