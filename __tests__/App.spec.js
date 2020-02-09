61b2690113e51ca37bf3ec210f7626f6cff964fd
naszzz <naszagorov@gmail.com>
2020-02-09 12:01:39 -0800

    updated tests

--- __tests__/App.spec.js
+++ __tests__/App.spec.js
@@ -3,22 +3,22 @@
 import React from 'react';
 import { shallow } from '../enzyme';
 
-import App from '../client/components/App';
+import DetailsApp from '../client/components/DetailsApp';
 import Details from '../client/components/Details';
 import Item from '../client/components/Item';
 //
-describe('App test', () => {
+describe('DetailsApp test', () => {
   it('should render', () => {
-    const wrapper = shallow(<App />);
+    const wrapper = shallow(<DetailsApp />);
     expect(wrapper.exists()).toBe(true);
   });
   it('should render Item and Details compnents', () => {
-    const wrapper = shallow(<App />);
+    const wrapper = shallow(<DetailsApp />);
     expect(wrapper.find(Details)).toBeDefined();
     expect(wrapper.find(Item)).toBeDefined();
   });
   it('should check componentDidMount()', () => {
-    const wrapper = shallow(<App />);
+    const wrapper = shallow(<DetailsApp />);
     const instance = wrapper.instance();
     jest.spyOn(instance, 'getDbData');
     instance.componentDidMount();
